44e38a79e74089adaed018c54ae2ebbd106e183f
Peashooter101 <Peashooter101@yahoo.com>
2023-01-14 17:50:46 -0800

    Added File Functionality
    Now supports the FILE saving-type.

--- src/main/java/adhdmc/simpleprefixes/util/saving/YMLFile.java
+++ src/main/java/adhdmc/simpleprefixes/util/saving/YMLFile.java
@@ -1,20 +1,38 @@
 package adhdmc.simpleprefixes.util.saving;
 
+import adhdmc.simpleprefixes.SimplePrefixes;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class YMLFile extends SaveHandler {
+    private final File dataFile = new File(SimplePrefixes.getPlugin().getDataFolder(), "prefix_data.yml");
+    private final FileConfiguration prefixData = new YamlConfiguration();
+
     @Override
     public void init() {
-
+        if (!dataFile.exists()) {
+            dataFile.getParentFile().mkdirs();
+            try { prefixData.save(dataFile); }
+            catch (IOException e) { e.printStackTrace(); }
+        }
+        try { prefixData.load(dataFile); }
+        catch (IOException | InvalidConfigurationException e) { e.printStackTrace(); }
     }
 
     @Override
     public String getPrefixId(OfflinePlayer p) {
-        return null;
+        return prefixData.getString(p.getUniqueId().toString(), null);
     }
 
     @Override
     public void setPrefixId(OfflinePlayer p, String id) {
-
+        prefixData.set(p.getUniqueId().toString(), id);
+        try { prefixData.save(dataFile); }
+        catch (IOException e) { e.printStackTrace(); }
     }
 }
